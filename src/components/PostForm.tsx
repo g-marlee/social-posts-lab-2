86d24101f134a9784a0941b5cfd11da04e5cf027
Marlee Garcia <marlee.garcia121@gmail.com>
2022-07-31 12:38:48 -0400

    Fixed x button on form modal

--- src/components/PostForm.tsx
+++ src/components/PostForm.tsx
@@ -3,6 +3,8 @@ import { nanoid } from 'nanoid';
 import React, { useState } from 'react';
 import Modal from 'react-modal';
 import { Post } from '../App';
+import { faHome, faXmarkCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 interface PostFormProps {
     openForm: boolean;
@@ -13,6 +15,7 @@ interface PostFormProps {
 
 
 export default function PostForm({openForm, onSubmit, onClose}: PostFormProps) {
+    
     const [post, setPost] = useState<Post>({title: '', thought: '', id: ''})
 
     function handleTitleChange(e: React.ChangeEvent<HTMLInputElement>) {
@@ -45,12 +48,12 @@ export default function PostForm({openForm, onSubmit, onClose}: PostFormProps) {
         <>
             <Modal isOpen={openForm} className="modal">
                 <form onSubmit={handleSubmit}>
+                    <button onClick={handleClose} className='close-button'><FontAwesomeIcon icon={faXmarkCircle} size={'lg'} /></button>
                     <label htmlFor="title" className='label'>Title</label>
                     <input type="text" className='title-input' name="title" placeholder='Title' value={post.title} required onChange={handleTitleChange}/>
                     <label htmlFor="thought" className='label'>Thought</label>
                     <textarea className='thought-input' name='thought' placeholder='Thought' value={post.thought} required onChange={handleThoughtChange}/>
-                    <div className='buttons'><button type='submit' className='form-button'>Post</button>
-                    <button onClick={handleClose} className='form-button'>Close</button></div>
+                    <button type='submit' className='post-button'>Post</button>
                 </form>
             </Modal>
         </>
